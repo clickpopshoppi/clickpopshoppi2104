8019b8952fa5899019cec26aac57cf11711ff2da
Click Pop Shop Pi <clickpopshoppi@gmail.com>
2025-10-13 02:03:38 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
+      marginTop: '100px'
+    }}>
+      <h1>🚀 Click Pop Shop Pi</h1>
+      <p>Welcome to the future of social commerce built on Pi Network 🌐</p>
+      <p>Developed by <b>RuamJaiRak Pi Network Thailand</b></p>
+    </div>
+  );
+}
