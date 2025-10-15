f69dad3e234b899ca763d79dda4e77a9ec08b9e4
Click Pop Shop Pi <clickpopshoppi@gmail.com>
2025-10-15 21:18:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "standalone"
 };
 
 module.exports = nextConfig;
