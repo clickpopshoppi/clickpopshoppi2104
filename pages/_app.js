72403d3be19c6530d1be90b44a9b5be87391fde2
Click Pop Shop Pi <clickpopshoppi@gmail.com>
2025-10-16 01:32:52 +0700

    Update Pi SDK loading and add Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // โหลด SDK จาก Pi Network
+    // โหลด Pi SDK จาก CDN ของ Pi Network
     const script = document.createElement("script");
     script.src = "https://sdk.minepi.com/pi-sdk.js";
     script.async = true;
     script.onload = () => {
       if (window.Pi) {
         window.Pi.init({ version: "2.0" });
-        console.log("✅ Pi SDK Loaded");
+        console.log("✅ Pi SDK Loaded Successfully!");
       } else {
         console.error("❌ Pi SDK not found");
       }
@@ -18,7 +19,14 @@ function MyApp({ Component, pageProps }) {
     document.body.appendChild(script);
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Click Pop Shop Pi</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
