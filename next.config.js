97cdffdebe05c6d57c9fe48477b9d632e248a867
Click Pop Shop Pi <clickpopshoppi@gmail.com>
2025-10-15 14:24:54 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,44 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: "standalone",
-  poweredByHeader: false,
-  compress: true,
-  images: {
-    domains: ["pi-network.github.io", "github.com", "vercel.app"],
-    formats: ["image/avif", "image/webp"],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          }
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
