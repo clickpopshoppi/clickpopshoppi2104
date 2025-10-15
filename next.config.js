4bdac8c6e87e74cc21fcaf288a0e21e2dff661c3
Click Pop Shop Pi <clickpopshoppi@gmail.com>
2025-10-15 14:46:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export", // ใช้สำหรับ static build
+  images: {
+    unoptimized: true // ปิด image optimization เพื่อรองรับ static export
+  }
 };
 
 module.exports = nextConfig;
